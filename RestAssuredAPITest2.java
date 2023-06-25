57995eae4cf38ccc2e7973cc1b9cddb420af8324
YogeshKolhe11 <yogeshkolhe11@gmail.com>
2023-06-25 13:04:36 +0530

    Rest assured sample test

---
+++ RestAssuredAPITest2.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class RestAssuredAPITest2 {
+
+    @Test
+    public void GetBooksDetails2() {
+        // Specify the base URL to the RESTful web service
+        RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+        // Get the RequestSpecification of the request to be sent to the server.
+        RequestSpecification httpRequest = RestAssured.given();
+        // specify the method type (GET) and the parameters if any.
+        //In this case the request does not take any parameters
+        Response response = httpRequest.request(Method.GET, "");
+        // Print the status and message body of the response received from the server
+        System.out.println("Status received => " + response.getStatusLine());
+        System.out.println("Response=>" + response.prettyPrint());
+
+    }
+}
